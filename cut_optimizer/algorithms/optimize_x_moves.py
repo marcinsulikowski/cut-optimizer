022fbeb0c52910d0a1a638bd99ffe8674d8a86a5
Marcin Sulikowski <marcin.k.sulikowski@gmail.com>
2020-06-14 20:15:47 +0200

    Add skeleton of the rest of the algorithm and tests
    We add functions stubs for all the steps of the algorithm. Its interface
    is now more or less complete so we also add a few tests which test cases
    which we found difficult when designing it. The test are obviously not
    passing yet so we mark all of them as "expected failure" (`xfail`).

--- cut_optimizer/algorithms/optimize_x_moves.py
+++ cut_optimizer/algorithms/optimize_x_moves.py
@@ -1,13 +1,21 @@
 """Minimizes the amount of X-axis moves needed to cut a set of polylines."""
 
-from typing import Iterable, List
+from typing import Iterable, List, Tuple, Union
 
+from cut_optimizer.algorithms.euler_path import euler_path
 from cut_optimizer.graph import Vertex
 from cut_optimizer.instance import Polyline
 from cut_optimizer.labelled_graph import LabelledGraph
 
 
-class XCoordGraph(LabelledGraph[int, Polyline]):
+class Penalty:
+    """Penalty which to be associated with idle moves of the cutter."""
+
+    def __init__(self, value: int) -> None:
+        self.value = value
+
+
+class XCoordGraph(LabelledGraph[int, Union[Polyline, Penalty]]):
     """Graph where vertices are X coordinates and edges and polylines."""
 
     def __init__(self) -> None:
@@ -35,6 +43,34 @@ class XCoordGraph(LabelledGraph[int, Polyline]):
             vertex = self._ensure_vertex(polyline.start.x)
             self.add_tagged_edge(vertex, vertex, polyline)
 
+    def remove_penalty_edges(self) -> None:
+        """Remove all penalty edges from the graph."""
+        penalty_edges = [
+            edge
+            for edge in self.edges
+            if isinstance(self.get_tag(edge), Penalty)
+        ]
+        for edge in penalty_edges:
+            self.remove_edge(edge)
+
+    def add_required_penalties(self, begin: Vertex, end: Vertex) -> None:
+        """Add penalty edges - phase 1."""
+
+    def make_connected(self) -> None:
+        """Add minimal edges to make the graph connected."""
+
+    def solve_for_end(self, path_end: Vertex) -> Tuple[int, List[Polyline]]:
+        """TODO."""
+        path_begin = self.get_vertex(0)
+        self.add_required_penalties(path_begin, path_end)
+        self.make_connected()
+        path = euler_path(self, path_begin)
+        tags = [self.get_tag(edge) for edge in path]
+        penalties = [tag.value for tag in tags if isinstance(tag, Penalty)]
+        polylines = [tag for tag in tags if isinstance(tag, Polyline)]
+        self.remove_penalty_edges()
+        return sum(penalties), polylines
+
     def _ensure_vertex(self, x_coordinate: int) -> Vertex:
         """Create vertex for a given X coordinate if not exists
 
@@ -51,4 +87,7 @@ def optimize_x_moves(polylines: List[Polyline]) -> List[Polyline]:
     graph = XCoordGraph()
     graph.add_open_polylines(poly for poly in polylines if poly.is_open)
     graph.add_closed_polylines(poly for poly in polylines if poly.is_closed)
-    return polylines
+    solutions = sorted(
+        [graph.solve_for_end(vertex) for vertex in graph.vertices]
+    )
+    return solutions[0][1]
---
+++ cut_optimizer/algorithms/test_optimize_x_moves.py
@@ -0,0 +1,50 @@
+"""Tests for optimize_x_moves.py."""
+
+from typing import Sequence
+
+import pytest
+
+from cut_optimizer.algorithms.optimize_x_moves import optimize_x_moves
+from cut_optimizer.instance import Point, Polyline
+
+
+def order_to_string(polylines: Sequence[Polyline]) -> str:
+    """Returns concatenated names of given polylines."""
+    return "".join(poly.name for poly in polylines)
+
+
+@pytest.mark.xfail
+def test_case_1() -> None:
+    """Test 1."""
+    polylines = [
+        Polyline("A", Point(1, 0), Point(7, 0), is_closed=False),
+        Polyline("B", Point(8, 0), Point(99, 0), is_closed=False),
+        Polyline("C", Point(9, 0), Point(20, 0), is_closed=False),
+    ]
+    assert order_to_string(optimize_x_moves(polylines)) == "ACB"
+
+
+@pytest.mark.xfail
+def test_case_2() -> None:
+    """Test 2."""
+    polylines = [
+        Polyline("L", Point(3, 0), Point(30, 0), is_closed=False),
+        Polyline("A", Point(1, 0), Point(2, 0), is_closed=False),
+        Polyline("B", Point(4, 0), Point(5, 0), is_closed=False),
+        Polyline("C", Point(33, 0), Point(34, 0), is_closed=False),
+        Polyline("D", Point(22, 0), Point(28, 0), is_closed=False),
+    ]
+    assert order_to_string(optimize_x_moves(polylines)) == "ABLDC"
+
+
+@pytest.mark.xfail
+def test_case_3() -> None:
+    """Test 3."""
+    polylines = [
+        Polyline("L", Point(3, 0), Point(30, 0), is_closed=False),
+        Polyline("A", Point(1, 0), Point(2, 0), is_closed=False),
+        Polyline("B", Point(4, 0), Point(5, 0), is_closed=False),
+        Polyline("C", Point(33, 0), Point(34, 0), is_closed=False),
+        Polyline("D", Point(11, 0), Point(28, 0), is_closed=False),
+    ]
+    assert order_to_string(optimize_x_moves(polylines)) == "ABCDL"
